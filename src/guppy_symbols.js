0317208fad48089edc2b8d44b10965d1ceabe6bc
DR <daniel3735928559@gmail.com>
2017-11-13 04:39:14 +0000

    more symbols cleanup

--- src/guppy_symbols.js
+++ src/guppy_symbols.js
@@ -1,22 +1,5 @@
 GuppySymbols = {"symbols":{}};
 
-GuppySymbols.symb_raw = function(symb_name,latex_symb,text_symb,group){
-    return {"output":{"latex":latex_symb,
-		      "text":text_symb},
-	    "group":group,
-	    "char":true,
-	    "type":symb_name};
-}
-
-var func_template = {"output":{"latex":"\\{$name}\\left({$1}\\right)",
-			       "text":" {$name}({$1})"},
-		     "type":"{$name}",
-		     "group":"{$group}",
-		     "attrs":[
-			 {"delete":"1"}
-		     ]
-		    };
-
 GuppySymbols.eval_template = function(template, name, args){
     args['name'] = name;
     if(Object.prototype.toString.call(template) == "[object String]"){
@@ -32,17 +15,6 @@ GuppySymbols.eval_template = function(template, name, args){
 	}
 	return template;
     }
-}
-
-GuppySymbols.symb_func = function(func_name,group){
-    return {"output":{"latex":"\\"+func_name+"\\left({$1}\\right)",
-		      "text":" " + func_name+"({$1})"},
-	    "type":func_name,
-	    "group":group,
-	    "attrs":[
-		{"delete":"1"}
-	    ]
-	   };
 }
 
 module.exports = GuppySymbols;
