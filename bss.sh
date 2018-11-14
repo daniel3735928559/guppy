15eb1bc7fd1a177c6c9e8fb4a71793d67b1dc816
DR <daniel3735928559@gmail.com>
2018-11-13 22:19:40 -0500

    building css

--- bss.sh
+++
@@ -1,4 +0,0 @@
-./node_modules/.bin/uglifycss lib/katex/katex-modified.min.css > build/guppy-none.min.css
-./node_modules/.bin/uglifycss lib/katex/katex-modified.min.css style/osk.css > build/guppy-none-osk.min.css
-./node_modules/.bin/uglifycss lib/katex/katex-modified.min.css style/guppy.css > build/guppy-default.min.css
-./node_modules/.bin/uglifycss lib/katex/katex-modified.min.css style/guppy.css style/osk.css > build/guppy-default-osk.min.css
